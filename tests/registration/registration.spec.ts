98c28aed5993e421e58d8c48d3c93dbaa98a38b5
Dmitry Shilovsky <wppbbh@gmail.com>
2023-02-25 21:21:01 +0400

    issue-7: delete wait

--- tests/registration/registration.spec.ts
+++ tests/registration/registration.spec.ts
@@ -1,12 +1,8 @@
 import test, { expect, Page } from "@playwright/test";
 import { RegistrationPage } from "../../src/components/pages/registration-page";
-import { timeout } from "../../src/support/utils";
 
 test.use({ viewport: { width: 1920, height: 1080 } });
 
-// Тесты связанные с регистрацией юзера
-// Позитивные: валидные данные для рестрации
-// Негативные: оставить форму пустой, невалидные данные для рестрации(спецзнаки в логине?, микропароль?) 
 test.describe(`Testing the entry positive and negative scenario`, async () => {
   let registrationPage: RegistrationPage;
 
@@ -21,18 +17,16 @@ test.describe(`Testing the entry positive and negative scenario`, async () => {
     await registrationPage.fillForm();
     await registrationPage.clickRegistrationButton();
 
-    expect(await registrationPage.getSuccessMessage()).
-    toBe('Your account was created successfully. You are now logged in.');
-   
-    expect(await registrationPage.getUserLogin()).
-    toBe(await registrationPage.getLoginFromWelcomeMessage());
+    expect(await registrationPage.getSuccessMessage()).toBe(
+      "Your account was created successfully. You are now logged in."
+    );
+
+    expect(registrationPage.getUserLogin()).toBe(
+      await registrationPage.getLoginFromWelcomeMessage()
+    );
   });
 
   test("Create new User for empty Data", async () => {
     await registrationPage.clickRegistrationButton();
-  })
+  });
 });
-
-
-
-
